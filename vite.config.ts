96ab437de79a89f6b277c28dbb7bc8752814fa88
0xshikhar <0xshikhar@gmail.com>
2025-08-15 03:51:21 +0700

    fix: route

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_MPC_FIRE_WATER_BASE ?? '/mpc-fire-water/',
+  base: process.env.VITE_MPC_FIRE_WATER_BASE ?? '/',
   plugins: [react()],
 });
